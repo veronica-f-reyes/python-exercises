429c36a2e584632d872dd97c4ee0bce1be466520
Veronica F Reyes <veronica.fierro.reyes1@gmail.com>
2021-07-12 12:12:39 -0500

    Adding to list_comprehensions work'

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -27,39 +27,101 @@ print(capitalized_fruits)
 
 # Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
 
-def vowelCounter(fruits):
-    vowels = 'aeiouAEIOU'
-    count = sum([1 for char in ''.join(fruits) if char in vowels])
-    return count
+vowels = 'aeiouAEIOU'
 
-fruits_with_more_than_two_vowels = []    
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if sum(letter in vowels for letter in fruit) > 2]      
+ 
+print(fruits_with_more_than_two_vowels )  
 
 # Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
 
+vowels = 'aeiouAEIOU'
+
+fruits_with_only_two_vowels = [fruit for fruit in fruits if sum(letter in vowels for letter in fruit) == 2]      
+ 
+print(fruits_with_only_two_vowels )
+
 # Exercise 5 - make a list that contains each fruit with more than 5 characters
+fruits_with_more_than_5_chars = [fruit for fruit in fruits if sum(letter in fruit for letter in fruit) > 5]
+
+print(fruits_with_more_than_5_chars)
 
 # Exercise 6 - make a list that contains each fruit with exactly 5 characters
 
+fruits_with_5_chars = [fruit for fruit in fruits if sum(letter in fruit for letter in fruit) == 5]
+
+print(fruits_with_5_chars)
+
 # Exercise 7 - Make a list that contains fruits that have less than 5 characters
+
+fruits_with_less_than_5_chars = [fruit for fruit in fruits if sum(letter in fruit for letter in fruit) < 5]
+
+print(fruits_with_less_than_5_chars)
 
 # Exercise 8 - Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
 
+chars_in_fruit = [sum(letter in fruit for letter in fruit) for fruit in fruits]
+
+print(chars_in_fruit)
+
 # Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
+
+a = 'a'
+
+fruits_with_letter_a =  [fruit for fruit in fruits if sum(letter in a for letter in fruit) >= 1 ] 
+
+print(fruits_with_letter_a)
 
 # Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
 
+even_numbers = [num for num in numbers if num % 2 == 0]
+
+print("Even numbers : ", even_numbers)
+
 # Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
+
+odd_numbers = [num for num in numbers if num % 2 != 0]
+
+print("Odd numbers : ", odd_numbers)
 
 # Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
 
+positive_numbers = [num for num in numbers if num > 0]
+
+print("Positive numbers : ", positive_numbers)
+
 # Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
+
+negative_numbers = [num for num in numbers if num < 0]
+
+print("Negative numbers : ", negative_numbers)
 
 # Exercise 14 - use a list comprehension w/ a conditional in order to produce a list of numbers with 2 or more numerals
 
+more_than_2_num =  [num for num in numbers if sum(chars in str(abs(num)) for chars in str(abs(num))) >= 2 ] 
+
+print("More than 2 numbers : ", more_than_2_num)
+
 # Exercise 15 - Make a variable named numbers_squared that contains the numbers list with each element squared. Output is [4, 9, 16, etc...]
+
+numbers_squared = [num**2 for num in numbers]
+
+print("Squared numbers : ", numbers_squared)
 
 # Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers that are both odd and negative.
 
+odd_negative_numbers = [num for num in numbers if (num < 0 and num % 2 != 0)]
+
+print("Odd negative numbers : ", odd_negative_numbers)
+
 # Exercise 17 - Make a variable named numbers_plus_5. In it, return a list containing each number plus five. 
 
+numbers_plus_5 = [num + 5 for num in numbers]
+
+print("Number plus five: ", numbers_plus_5)
+
 # BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers list. *Hint* you may want to make or find a helper function that determines if a given number is prime or not.
+
+primes = [num for num in numbers if 0 not in [num%i for i in range(2,int(num/2)+1)]]
+
+print (primes)
